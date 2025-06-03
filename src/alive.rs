6362706d56b13fce58386a80c502073738f017ed
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-06-03 10:24:21 +0800

    fix: cleanup

---
+++ src/alive.rs
@@ -0,0 +1,43 @@
+// Use to check if server is alive or not
+use anyhow::Error;
+use reqwest::Client;
+use tokio::task::JoinHandle;
+
+pub fn handle_alive(rpc: String, url: String, name: String) -> Result<JoinHandle<()>, Error> {
+    let j = tokio::spawn(async move {
+        loop {
+            if let Err(err) = check_alive(rpc.clone(), url.clone(), name.clone()).await {
+                eprintln!("Error checking alive for {:?}: {}", name, err);
+            }
+
+            tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+        }
+    });
+
+    Ok(j)
+}
+
+pub(crate) async fn check_alive(rpc: String, url: String, name: String) -> Result<(), Error> {
+    let client = Client::new();
+
+    let req = client.get(rpc.as_str()).send().await?;
+
+    let is_success = req.status().is_success();
+    if is_success {
+        println!("RPC alive");
+    } else {
+        return Err(Error::msg("RPC failed to respond"));
+    }
+
+    let client_ping = Client::new();
+
+    let req_ping = client_ping.get(url.as_str()).send().await?;
+
+    let is_success = req_ping.status().is_success();
+    if is_success {
+        println!("Status posted successfully");
+    } else {
+        eprintln!("Error posting status for {}", name);
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,18 @@
-use std::time::Duration;
+mod alive;
 
+use alive::handle_alive;
 use anyhow::Error;
 use dotenv::dotenv;
-use reqwest::Client;
 use std::env;
-use tokio_cron_scheduler::{Job, JobScheduler};
-
-async fn ping_status(url: &String) -> Result<(), Error> {
-    let client = Client::new();
-
-    let req = client.get(url.as_str()).send().await?;
-
-    let is_success = req.status().is_success();
-    if is_success {
-        println!("Status posted successfully");
-    } else {
-        return Err(Error::msg("Failed to send status"));
-    }
-    Ok(())
-}
-
-pub async fn check_alive(rpc: &String, url: &String, name: &String) -> Result<(), Error> {
-    let client = Client::new();
-
-    let req = client.get(rpc.as_str()).send().await?;
-
-    let is_success = req.status().is_success();
-    if is_success {
-        println!("RPC alive");
-    } else {
-        return Err(Error::msg("RPC failed to respond"));
-    }
-    // TODO: Handle Catchup case
-    // ----------- Ping status
-    if let Err(err) = ping_status(&url).await {
-        eprintln!("Error posting status for {}: {}", name, err);
-    }
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv().ok();
-    let sched = JobScheduler::new()
-        .await
-        .expect("Failed to create JobScheduler");
+    let url = env::var("PING_URL").expect("PING_URL environment variable not set");
     let name = env::var("SERVICE_NAME").expect("SERVICE_NAME environment variable not set");
-    println!("Starting Service for {}", name);
-    // Create a job that calls post_status periodically.
-    // Replace "http://example.com" with your actual URL.
-    let job = match Job::new_repeated_async(Duration::from_secs(10), |_uuid, _lock| {
-        Box::pin(async move {
-            let url = env::var("PING_URL").expect("PING_URL environment variable not set");
-            let name = env::var("SERVICE_NAME").expect("SERVICE_NAME environment variable not set");
-            let rpc = env::var("RPC_URL").expect("RPC_URL environment variable not set");
-            if let Err(err) = check_alive(&rpc, &url, &name).await {
-                eprintln!("Error in live check for {}: {}", name, err);
-            }
-        })
-    }) {
-        Ok(r) => r,
-        Err(e) => return Err(Error::msg(e)),
-    };
+    let rpc = env::var("RPC_URL").expect("RPC_URL environment variable not set");
+    let join_handle = handle_alive(rpc.clone(), url.clone(), name.clone()).unwrap();
 
-    sched.add(job).await.expect("Failed to add job");
-    sched.start().await.expect("Failed to start scheduler");
-
-    tokio::time::sleep(Duration::from_secs(100)).await;
-
+    join_handle.await?;
     Ok(())
 }
